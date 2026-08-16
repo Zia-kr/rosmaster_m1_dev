5e8597640dfb97eb66aa10b8f4c869e56e877644
Ziad-kr <ziadkriedieh@gmail.com>
2026-08-16 10:28:03 +0300

    feature : first version of watchdow timer

--- src/teleop_pkg/teleop_pkg/key_command_node.py
+++ src/teleop_pkg/teleop_pkg/key_command_node.py
@@ -2,6 +2,8 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from Rosmaster_Lib import Rosmaster
+import time
+
 
 
 class CommandSubscriber(Node):
@@ -15,10 +17,24 @@ class CommandSubscriber(Node):
             10)
         self.subscription  
         self.bot = Rosmaster(com="/dev/ttyUSB1")
+        watchdog_timer_period = 0.1
+        self.timer = self.create_timer(watchdog_timer_period, self.timer_callback)
+        self.last_command_time = 0.0
+
+
 
     def listener_callback(self, msg):
+        self.last_command_time = time.monotonic()
         self.bot.set_car_motion(msg.linear.x, msg.linear.y, msg.angular.z)
         self.get_logger().info(f'linear_x: {msg.linear.x}, linear_y: {msg.linear.y}, angular_z: {msg.angular.z}')
+    
+    def timer_callback(self):
+        current_time = time.monotonic()
+        if current_time - self.last_command_time > 0.8:
+            self.bot.set_car_motion(0.0, 0.0, 0.0)
+            self.get_logger().info('No command received for 0.1 seconds, stopping the robot.')
+        else:
+            self.get_logger().info('Command received, robot is moving.')
 
 
 def main(args=None):
